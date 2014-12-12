d5714f409c2689f4793f610201cde13a32b50b09
Lubbert Kramer <lkramer@ict-zwolle.nl>
2014-12-12 11:54:03 +0100

    Plan van Aanpak en functioneel ontwerp besproken, test nummer bij crawler gedaan.

--- view/content/crawler.php
+++ view/content/crawler.php
@@ -1,6 +1,9 @@
 <form action='<?php echo URL ?>crawler/curl' method='post'>
     <input type='text' name='number'>
-</form>
+</form><br>
+<p>
+    Test nummer: 9812355
+</p>
 <?php
     if(isset($this->data)) {
         echo $this->data;
