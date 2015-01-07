d278dc087f9fa896befde8bd95caaf4da69dcf82
Lubbert Kramer <lkramer@ict-zwolle.nl>
2015-01-07 13:07:50 +0100

    Plan van Aanpak en functioneel ontwerp besproken, test nummer bij crawler gedaan.

--- controller/crawler.class.php
+++ controller/crawler.class.php
@@ -4,10 +4,9 @@ class Crawler extends Controller {
 
     public function __construct() {
         parent::__construct();
-        $title = 'crawler';
-        $style = 'bootstrap';
-        $this->view->title = $title;
-        $this->view->style = $style;
+        //$this->view->script = "crawler";
+        $this->view->title = 'crawler';
+        $this->view->style = 'bootstrap';
     }
 
     function index() {
@@ -15,19 +14,31 @@ class Crawler extends Controller {
     }
 
     function curl() {
-        if(isset($_POST['number'])) {
+        if(isset($_POST['number']) && $_POST['richting'] == "ecabo"){
+            if($_POST['number'] == ""){
+                header("location: ".URL."crawler");
+            }
             include 'external/simple_html_dom.php';
             $html = new simple_html_dom();
-
-            $html = file_get_html('http://mijn.ecabo.nl/zoeken/Leerbedrijf/Index/'.$_POST['number']);
+            $html = file_get_html('http://mijn.ecabo.nl/zoeken/Leerbedrijf/Index/' . $_POST['number']);
             // Find all links
+            //$returnval = "";
             foreach ($html->find('.content-holder') as $element) {
-                $returnval = $element . '<br>';
+                $returnval = $element;
             }
-            $this->view->data = $returnval;
+            if(isset($returnval->children) == null){
+                header("location: ".URL."crawler");
+            }
+            $test = $returnval->children(0);
+            $test .= $returnval->children(2);
+            $test .= $returnval->children(4);
+            $test .= $returnval->children(8);
+            $this->view->data = $test;
             $this->view->render('crawler');
-        }else {
-            return;
+        }else if(isset($_POST['number']) && $_POST['richting'] == "goc"){
+            $this->view->render('crawler');
+            echo "JE KUT!";
         }
     }
+
 }
--- view/content/crawler.php
+++ view/content/crawler.php
@@ -1,11 +1,14 @@
 <form action='<?php echo URL ?>crawler/curl' method='post'>
+    <select name='richting'>
+        <option value="ecabo">ECABO</option>
+        <option value="goc">GOC</option>
+    </select>
     <input type='text' name='number'>
 </form><br>
 <p>
     Test nummer: 9812355
 </p>
 <?php
-    if(isset($this->data)) {
-        echo $this->data;
-    }
-?>
+if (isset($this->data)) {
+    echo $this->data;
+}
