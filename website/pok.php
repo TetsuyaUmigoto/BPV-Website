43ff175f8f6f8f570678ef69ab87d2c474592140
Your full name <roosmarijn@MacBook-Pro-van-Roosmarijn.local>
2015-01-07 15:27:56 +0100

    nieuw wireframe's en een css

---
+++ website/pok.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<title>pok-aanvraag</title>
+</head>
+<body>
+	<div class="wrap">
+		<div class="header">start</div>
+		<div class="vraag">vraag</div>
+		<div class="antwoord">antwoord</div>
+	</div>
+</body>
+</html>
