9cce0e5b47053828762c89e07cb8b072be3bcc12
Your full name <roosmarijn@MacBook-Pro-van-Roosmarijn.local>
2015-01-21 13:25:50 +0100

    lerarenpagina en style.css

--- website/lerarenpagina.php
+++ website/lerarenpagina.php
@@ -7,7 +7,8 @@
 </head>
 <body>
 	<div class="header"><h1><img src="../docs/PSD/logo.png">   header</h1></div>
-	div.
+	<div class="hoi1">hoi1</div>
+	<div class="nee">hoi</div>
 	<div class="footer">footer</div>
 </body>
 </html>
