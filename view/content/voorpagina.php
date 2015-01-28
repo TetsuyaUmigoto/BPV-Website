cfb0340ff21eee19a6382cc67cc43a74cca6ca36
Lubbert Kramer <lkramer@ict-zwolle.nl>
2015-01-28 09:11:12 +0100

    few fixxes

--- view/content/voorpagina.php
+++ view/content/voorpagina.php
@@ -1,6 +1,6 @@
 <div class="wrap">
     <div class="menu">
-        <div class="logo"><img src="../docs/PSD/logo.png"></div>
+        <div class="logo"><img src="<?php echo URL ?>public/img/logo.png"></div>
         <div class="stage">stage's</div>
         <div class="inloggen">inloggen</div></div>
     <div class="doel">doel site</div>
