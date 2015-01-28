2ff6db000b2db50d1b07f290da53c7245617b090
Lubbert Kramer <lkramer@ict-zwolle.nl>
2015-01-28 13:07:24 +0100

    Added database sql

--- bpv-website.sql
+++ bpv-website.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Genereertijd: 26 jan 2015 om 10:08
+-- Genereertijd: 28 jan 2015 om 13:06
 -- Serverversie: 5.6.14
 -- PHP-versie: 5.5.6
 
@@ -23,6 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `bedrijven`
+--
+
+CREATE TABLE IF NOT EXISTS `bedrijven` (
+  `bedrijf_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ref_nummer` int(11) NOT NULL,
+  `soort` int(1) NOT NULL,
+  `wachtwoord` varchar(256) NOT NULL,
+  `bedrijf_naam` varchar(25) NOT NULL,
+  `omschrijving` varchar(500) NOT NULL,
+  PRIMARY KEY (`bedrijf_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `calender`
+--
+
+CREATE TABLE IF NOT EXISTS `calender` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `afspraak` varchar(500) NOT NULL,
+  `leerlingnummer` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `comments`
 --
 
@@ -32,19 +62,39 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment` varchar(500) NOT NULL,
   `rating` int(1) NOT NULL,
   PRIMARY KEY (`comment_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `leraren`
+--
+
+CREATE TABLE IF NOT EXISTS `leraren` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `voornaam` varchar(25) NOT NULL,
+  `achternaam` varchar(25) NOT NULL,
+  `wachtwoord` varchar(256) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `leerlingen`
+-- Tabelstructuur voor tabel `studenten`
 --
 
-CREATE TABLE IF NOT EXISTS `leerlingen` (
+CREATE TABLE IF NOT EXISTS `studenten` (
   `leerlingnummer` int(11) NOT NULL,
-  `naam` varchar(25) NOT NULL,
+  `voornaam` varchar(25) NOT NULL,
   `achternaam` varchar(25) NOT NULL,
-  `wachtwoord` varchar(50) NOT NULL,
+  `wachtwoord` varchar(256) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `geboortedatum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `telefoonnummer` int(11) NOT NULL,
+  `klas` varchar(11) NOT NULL,
+  `leraar_id` int(11) NOT NULL,
   PRIMARY KEY (`leerlingnummer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- view/content/voorpagina.php
+++ view/content/voorpagina.php
@@ -2,7 +2,9 @@
     <div class="menu">
         <div class="logo"><img src="<?php echo URL ?>public/img/logo.png"></div>
         <div class="stage">stage's</div>
-        <div class="inloggen">inloggen</div></div>
+        <div class="inloggen">inloggen</div>
+    </div>
+
     <div class="doel">doel site</div>
     <div class="footer">footer</div>
 </div>
