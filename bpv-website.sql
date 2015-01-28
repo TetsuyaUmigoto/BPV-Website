6b1ce4861610deedb7ca4ce3d1281a03ec7581af
Jonas <Jonas@10.0.1.228>
2015-01-28 15:41:45 +0100

    SQL update

--- bpv-website.sql
+++ bpv-website.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Genereertijd: 28 jan 2015 om 13:06
--- Serverversie: 5.6.14
--- PHP-versie: 5.5.6
+-- Gegenereerd op: 28 jan 2015 om 15:40
+-- Serverversie: 5.6.20
+-- PHP-versie: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,14 +27,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `bedrijven` (
-  `bedrijf_id` int(11) NOT NULL AUTO_INCREMENT,
+`bedrijf_id` int(11) NOT NULL,
   `ref_nummer` int(11) NOT NULL,
   `soort` int(1) NOT NULL,
   `wachtwoord` varchar(256) NOT NULL,
   `bedrijf_naam` varchar(25) NOT NULL,
-  `omschrijving` varchar(500) NOT NULL,
-  PRIMARY KEY (`bedrijf_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `omschrijving` varchar(500) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `bedrijven`
+--
+
+INSERT INTO `bedrijven` (`bedrijf_id`, `ref_nummer`, `soort`, `wachtwoord`, `bedrijf_naam`, `omschrijving`) VALUES
+(1, 0, 0, '098f6bcd4621d373cade4e832627b4f6', '', '');
 
 -- --------------------------------------------------------
 
@@ -43,11 +49,10 @@ CREATE TABLE IF NOT EXISTS `bedrijven` (
 --
 
 CREATE TABLE IF NOT EXISTS `calender` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `afspraak` varchar(500) NOT NULL,
-  `leerlingnummer` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `leerlingnummer` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -57,11 +62,10 @@ CREATE TABLE IF NOT EXISTS `calender` (
 --
 
 CREATE TABLE IF NOT EXISTS `comments` (
-  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+`comment_id` int(11) NOT NULL,
   `leerlingnummer` int(11) NOT NULL,
   `comment` varchar(500) NOT NULL,
-  `rating` int(1) NOT NULL,
-  PRIMARY KEY (`comment_id`)
+  `rating` int(1) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
@@ -71,13 +75,19 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `leraren` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `voornaam` varchar(25) NOT NULL,
   `achternaam` varchar(25) NOT NULL,
   `wachtwoord` varchar(256) NOT NULL,
-  `email` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `email` varchar(25) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `leraren`
+--
+
+INSERT INTO `leraren` (`id`, `voornaam`, `achternaam`, `wachtwoord`, `email`) VALUES
+(1, 'Jonas', '', '098f6bcd4621d373cade4e832627b4f6', '');
 
 -- --------------------------------------------------------
 
@@ -94,10 +104,75 @@ CREATE TABLE IF NOT EXISTS `studenten` (
   `geboortedatum` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `telefoonnummer` int(11) NOT NULL,
   `klas` varchar(11) NOT NULL,
-  `leraar_id` int(11) NOT NULL,
-  PRIMARY KEY (`leerlingnummer`)
+  `leraar_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Gegevens worden geëxporteerd voor tabel `studenten`
+--
+
+INSERT INTO `studenten` (`leerlingnummer`, `voornaam`, `achternaam`, `wachtwoord`, `email`, `geboortedatum`, `telefoonnummer`, `klas`, `leraar_id`) VALUES
+(0, 'Jonas', 'Muilwijk', '098f6bcd4621d373cade4e832627b4f6', '', '2015-01-28 13:31:12', 0, '', 0),
+(1, 'jonas', '', '098f6bcd4621d373cade4e832627b4f6', '', '2015-01-28 14:23:01', 0, '', 0);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `bedrijven`
+--
+ALTER TABLE `bedrijven`
+ ADD PRIMARY KEY (`bedrijf_id`);
+
+--
+-- Indexen voor tabel `calender`
+--
+ALTER TABLE `calender`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `comments`
+--
+ALTER TABLE `comments`
+ ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexen voor tabel `leraren`
+--
+ALTER TABLE `leraren`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `studenten`
+--
+ALTER TABLE `studenten`
+ ADD PRIMARY KEY (`leerlingnummer`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `bedrijven`
+--
+ALTER TABLE `bedrijven`
+MODIFY `bedrijf_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT voor een tabel `calender`
+--
+ALTER TABLE `calender`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `comments`
+--
+ALTER TABLE `comments`
+MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT voor een tabel `leraren`
+--
+ALTER TABLE `leraren`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
