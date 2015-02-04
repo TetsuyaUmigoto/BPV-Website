489dd8245ced863a554633f10cfa4de843433a6f
Jonas <Jonas@10.0.1.125>
2015-02-04 14:23:28 +0100

    Testcommit

--- view/content/overzicht.php
+++ view/content/overzicht.php
@@ -1,7 +1,6 @@
 <div class="wrap">
     <div class="zoek"> <p>zoekbalk</p> </div>
     <div class="bedrijf"><p>bedrijf</p>
-        <div class="bedrijf1"> <p>bedrijf1</p></div>
-        <div class="bedrijf2"> <p>bedrijf2</p></div>
+        <div class="bedrijf"><p>bedrijf1</p></div>
     </div>
 </div>
