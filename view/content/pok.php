cdafdd59482e4cf040b4c4ab3e100f237874a395
Your full name <roosmarijn@MacBook-Pro-van-Roosmarijn.local>
2015-02-11 14:16:29 +0100

    no message

--- view/content/pok.php
+++ view/content/pok.php
@@ -2,55 +2,38 @@
     <div class="pok">
 			<FORM METHOD="post" ACTION="" >
 <DIV>
-<h3>Student</h3>
-	<table>
-		<tr><td>Naam</td><td>:</td><td><input name="test2" type="text" size="30" value=""></td> </tr>
-		<tr><td>Tussenvoegsels <td>:</td><td><input name="test2" type="text" size="30" value=""></td> </tr>
-		<tr><td>Voorletters</td> <td>:</td> <td><input name="test3" type="text" size="30" value=""></td> </tr>
-		<tr><td>Roepnaam</td><td>:</td><td><input name="test4" type="text" size="30"value=""></td> </tr>
-		<tr><td>Geslacht <td>:</td><td><input name="test5" type="text" size="30" value=""></td> </tr>
-		<tr><td>Geboortedatum</td> <td>:</td> <td><input name="test6" type="text" size="30" value=""></td> </tr>
-		<tr><td>Adres</td><td>:</td><td><input name="test7" type="text" size="30"value=""></td> </tr>
-		<tr><td>Telefoon <td>:</td><td><input name="test8" type="tex" size="30" value=""></td> </tr>
-		<tr><td>Burgerservicenummer</td> <td>:</td> <td><input name="test9" type="text" size="30" value=""></td> </tr>
-		<tr><td>Studentennummer</td><td>:</td><td><input name="test10" type="text" size="30" value=""></td> </tr>
-		<tr><td>Nationaliteit <td>:</td><td><input name="test11" type="text" size="30" value=""></td> </tr>
-		<tr><td>Plaats</td> <td>:</td> <td><input name="test12" type="text" size="30" value=""></td> </tr>
-	</table>
 	<h3>Beroepspraktijkvorming biedende organisatie</h3>
 		<table>	
-			<tr><td>Naam</td><td>:</td> <td> <input name="test13." type="text" size="30"value=""></td> </tr>
-			<tr> <td>Praktijkadres </td>  <td>:</td> <td> <input name="test14" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Correspondentieadres</td>  <td>:</td> <td> <input name="test15" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Telefoon, fax</td><td>:</td> <td> <input name="test16" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Bedrijfscode</td><td>:</td> <td> <input name="test17" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Praktijkbegeleider</td><td>:</td> <td> <input name="test18" type="text" size="30"value=""></td> </tr> </table>
-			<h4>Rechtsgeldig vertegenwooordigd in de persoon van</h4> <table>
-			<tr> <td>Naam</td><td>:</td> <td> <input name="test19" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Plaats</td><td>:</td> <td> <input name="test20" type="text" size="30"value=""></td> </tr>
+			<tr><td>Bedrijfsnaam</td><td>:</td> <td> <input name="test1." type="text" size="30"value=""></td> </tr>
+			<tr> <td>Adres </td>  <td>:</td> <td> <input name="test2" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Postcode </td>  <td>:</td> <td> <input name="test3" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Telefoon</td><td>:</td> <td> <input name="test4" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Website </td>  <td>:</td> <td> <input name="test5" type="text" size="30"value=""></td> </tr>
+
+			<tr> <td>Contactpersoon+voorletters</td><td>:</td> <td> <input name="test6" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Contactpersoon telefoon</td><td>:</td> <td> <input name="test7" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Contactpersoon @-mail</td><td>:</td> <td> <input name="test8" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Praktijkbegeleider+ voorletter(s)</td><td>:</td> <td> <input name="test9" type="text" size="30"value=""></td> </tr>
+			<tr> <td>Praktijkbegeleider telefoon </td>  <td>:</td> <td> <input name="test10" type="text" size="30"value=""></td> </tr>
+			<tr><td>Kenniscentrum </td><td>:</td> <td> <input name="test11" type="text" size="30"value=""></td> </tr>
+			<tr><td>Bedrijfscode van accreditatie </td><td>:</td> <td> <input name="test12" type="text" size="30"value=""></td> </tr>
 		</table>
-	<h3>Landstede</h3>
-		<table>
-			<tr> <td>Adres</td><td>:</td> <td> <input name="test21" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Correspondentieadres</td><td>:</td> <td> <input name="test22" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Onderwijsbegeleider</td><td>:</td> <td> <input name="test23" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Telefoon</td><td>:</td> <td> <input name="test24" type="text" size="30"value=""></td> </tr>
-		</table>
-	<h4>Rechtsgeldig vertegenwoordigd in de persoon van</h4>
-		<table>
-			<tr> <td>Naam</td><td>:</td> <td> <input name="test25" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Plaats</td><td>:</td> <td> <input name="test26" type="text" size="30"value=""></td> </tr>
-		</table>
-	<h3>Verklaren het volgende te zijn overgeengekomen</h3>
-		<table>
-			<tr> <td>Naam opleiding</td><td>:</td> <td> <input name="test27" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Crebo-code OC&W</td><td>:</td> <td> <input name="test28" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Leerweg</td><td>:</td> <td> <input name="test29" type="text" size="30"value="BOL"></td> </tr>
-			<tr> <td>Klokuren BPV totaal</td><td>:</td> <td> <input name="test30" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Begin BPV</td><td>:</td> <td> <input name="test31" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Kwalificatieniveaau</td><td>:</td> <td> <input name="test32" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Aantal klokuren per week</td><td>:</td> <td> <input name="test33" type="text" size="30"value=""></td> </tr>
-			<tr> <td>Eind BPV</td><td>:</td> <td> <input name="test34" type="text" size="30"value=""></td> </tr>
+		<h3>Student</h3>
+	<table>
+		<tr><td>Naam</td><td>:</td><td><input name="tes13" type="text" size="30" value=""></td> </tr>
+		<tr><td>Studentnummer <td>:</td><td><input name="test14" type="tex" size="30" value=""></td> </tr>
+		<tr><td>Klascode</td> <td>:</td> <td><input name="test15" type="text" size="30" value=""></td> </tr>
+		<tr><td>Opleidingsnaam</td><td>:</td><td><input name="test16" type="text" size="30" value=""></td> </tr>
+		<tr><td>Crebonummer <td>:</td><td><input name="test17" type="text" size="30" value=""></td> </tr>
+		<tr><td>BOL/BBL</td> <td>:</td> <td><input name="test18" type="text" size="30" value=""></td> </tr>
+		<tr><td>Inleverdatum</td>  <td>:</td> <td> <input name="test19" type="text" size="30"value=""></td> </tr>			
+		<tr> <td>Bpv-coördinator</td><td>:</td> <td> <input name="test20" type="text" size="30"value=""></td> </tr>
+		<tr> <td>Stagebegeleider Landstede</td><td>:</td> <td> <input name="test21" type="text" size="30"value=""></td> </tr>
+		<tr> <td>BPV-periode (data)</td><td>:</td> <td> <input name="test22" type="text" size="30"value=""></td> </tr>
+		<tr> <td>Aantal sbu's</td><td>:</td> <td> <input name="test23" type="text" size="30"value=""></td> </tr>
+		<tr> <td>Brinnummer</td><td>:</td> <td> <input name="test24" type="text" size="30"value=""></td> </tr>
+		<tr> <td>Crebonummer</td><td>:</td> <td> <input name="test25" type="text" size="30"value=""></td> </tr>
+		<tr> <td>Opmerking</td><td>:</td> <td> <input name="test26" type="text" size="30"value=""></td> </tr>
 		</table>
 
 	<br><br>
