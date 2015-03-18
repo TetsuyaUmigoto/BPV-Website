54d01164cddd176759db419c0fd6a4fec5925cc2
Your full name <roosmarijn@MacBook-Pro-van-Roosmarijn.local>
2015-03-18 13:59:15 +0100

    style css. voorpagina.

--- view/content/voorpagina.php
+++ view/content/voorpagina.php
@@ -1,3 +1,3 @@
 <div id="wrap">
-    <div id="doel">doel site</div>
+    <div id="doel">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore possimus, doloremque tempora dicta, quisquam dolorum quae vero nulla excepturi quo quis odio temporibus deserunt ex quod et doloribus nisi! At!</div>
 </div>
