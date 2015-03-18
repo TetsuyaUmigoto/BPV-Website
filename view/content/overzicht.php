1962bbdbf6d3d68d7349387e17253db83aead45e
Lubbert Kramer <lkramer@ict-zwolle.nl>
2015-03-18 13:19:26 +0100

    Overzicht pagina

--- view/content/overzicht.php
+++ view/content/overzicht.php
@@ -1,31 +1,29 @@
 <div class="wrap">
-    <table>
-        <tr>
-   <td> <div id="zoek"> <p>zoekbalk</p> </div></td>
-    <td><div id="bedrijf"></td>
-        </tr>
-        </table>
+    <div id="zoek">
+	<p>zoekbalk</p>
+    </div>
+    <div id="bedrijf">
         <table id="overzichtBedrijven">
             <thead>
-                <td>
-                    Ecabo/GOC nummer
-                </td>
-                <td>
-                    Naam
-                </td>
+	    <td>
+		Ecabo/GOC nummer
+	    </td>
+	    <td>
+		Naam
+	    </td>
             </thead>
-        <?php 
-            foreach($this->bedrijfList as $bedrijf){
-                echo "<tr>"
-                    . "<td>" 
-                        . $bedrijf['bedrijf_id'] . 
-                    "</td>"
-                    . "<td>"
-                        . "<a href='" . URL . "bedrijf/showBedrijf/" . $bedrijf["bedrijf_id"] . "'>" . $bedrijf["bedrijf_naam"] . "</a>" . 
-                    "</td>" .
-                "</tr>";
-            }
-        ?>
+	    <?php
+	    foreach($this->bedrijfList as $bedrijf){
+		echo "<tr>"
+		. "<td>"
+		. $bedrijf['bedrijf_id'] .
+		"</td>"
+		. "<td>"
+		. "<a href='" . URL . "bedrijf/showBedrijf/" . $bedrijf["bedrijf_id"] . "'>" . $bedrijf["bedrijf_naam"] . "</a>" .
+		"</td>" .
+		"</tr>";
+	    }
+	    ?>
         </table>
     </div>
 </div>
