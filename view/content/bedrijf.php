84031a3f8f70f685b3c7304e22a9c22dc8577cd9
Your full name <roosmarijn@MacBook-Pro-van-Roosmarijn.local>
2015-04-02 10:28:31 +0200

    maps show bedrijf.

--- view/content/bedrijf.php
+++ view/content/bedrijf.php
@@ -7,9 +7,59 @@
                 <div class="bedrijfInfo"><p><?php echo $info['bedrijf_naam'] ?></p></div>
                 <div class="bedrijfInfo"><p>ster</p></div>
                 <div class="bedrijfInfo">
-		    <div id="map" style="width: 500px; height: 400px;">
 
-		    </div>
+
+		</div>
+                <div class="bedrijfInfo"><p>commentaar</p></div>
+                <div class="bedrijfInfo"><p><?php echo $this->gemiddelde ?> Sterren</p></div>
+                <?php
+            }
+        }
+        ?>
+    </div>
+
+    <div id="gebruikerComments">
+        <?php
+        if (isset($this->comments)) {
+            foreach ($this->comments as $row) {
+                echo
+                " <div id='rating'>" . $row['rating'] . "</div>"
+                . "<div id='leerlingnummer'>" . $row['leerlingnummer'] . "</div>"
+                . "<div id='comment'>" . $row['comment'] . "</div>";
+            }
+        }
+        ?>
+    </div>
+    <div class="bedrijf-comments-test">
+        <form action='<?php echo URL ?>bedrijf/postComment' method='post'>
+            <input type="hidden" name="bedrijf_id" value="<?php echo $this->bedrijfId ?>" />
+            <input type="hidden" name="rating" id="ratingInput" value="" />
+            LeerlingNummer:<input type='text' class='form-control' name='leerlingnummer'>
+            Comment:<input type='text' class='form-control' name='comment'>
+            Rating:<br><span class="rating">
+                <input type="radio" class="rating-input"
+                       id="rating-input-5" name="rating-input-1">
+                <label for="rating-input-5" class="rating-star"></label>
+                <input type="radio" class="rating-input"
+                       id="rating-input-4" name="rating-input-1">
+                <label for="rating-input-4" class="rating-star"></label>
+                <input type="radio" class="rating-input"
+                       id="rating-input-3" name="rating-input-1">
+                <label for="rating-input-3" class="rating-star"></label>
+                <input type="radio" class="rating-input"
+                       id="rating-input-2" name="rating-input-1">
+                <label for="rating-input-2" class="rating-star"></label>
+                <input type="radio" class="rating-input"
+                       id="rating-input-1" name="rating-input-1">
+                <label for="rating-input-1" class="rating-star"></label>
+            </span>
+            <input type='submit' class='btn btn-primary' value='Comment!'>
+        </form>
+    </div>
+</div>
+		    <div class="map2"><div id="map"  style="width: 500px; height: 400px;">
+
+
 		    <script type="text/javascript"
 			    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGBByvGgOTRGqO-ZB1hVoAeVZGjm6UoTM">
 		    </script>
@@ -73,50 +123,4 @@
 			}
 			theNext();
 		    </script>
-		</div>
-                <div class="bedrijfInfo"><p>commentaar</p></div>
-                <div class="bedrijfInfo"><p><?php echo $this->gemiddelde ?> Sterren</p></div>
-                <?php
-            }
-        }
-        ?>
-    </div>
-    <div id="gebruikerComments">
-        <?php
-        if (isset($this->comments)) {
-            foreach ($this->comments as $row) {
-                echo
-                " <div id='rating'>" . $row['rating'] . "</div>"
-                . "<div id='leerlingnummer'>" . $row['leerlingnummer'] . "</div>"
-                . "<div id='comment'>" . $row['comment'] . "</div>";
-            }
-        }
-        ?>
-    </div>
-    <div class="bedrijf-comments-test">
-        <form action='<?php echo URL ?>bedrijf/postComment' method='post'>
-            <input type="hidden" name="bedrijf_id" value="<?php echo $this->bedrijfId ?>" />
-            <input type="hidden" name="rating" id="ratingInput" value="" />
-            LeerlingNummer:<input type='text' class='form-control' name='leerlingnummer'>
-            Comment:<input type='text' class='form-control' name='comment'>
-            Rating:<br><span class="rating">
-                <input type="radio" class="rating-input"
-                       id="rating-input-5" name="rating-input-1">
-                <label for="rating-input-5" class="rating-star"></label>
-                <input type="radio" class="rating-input"
-                       id="rating-input-4" name="rating-input-1">
-                <label for="rating-input-4" class="rating-star"></label>
-                <input type="radio" class="rating-input"
-                       id="rating-input-3" name="rating-input-1">
-                <label for="rating-input-3" class="rating-star"></label>
-                <input type="radio" class="rating-input"
-                       id="rating-input-2" name="rating-input-1">
-                <label for="rating-input-2" class="rating-star"></label>
-                <input type="radio" class="rating-input"
-                       id="rating-input-1" name="rating-input-1">
-                <label for="rating-input-1" class="rating-star"></label>
-            </span>
-            <input type='submit' class='btn btn-primary' value='Comment!'>
-        </form>
-    </div>
-</div>
+		    		    </div></div>
