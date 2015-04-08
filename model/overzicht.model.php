4dd2486fd0f1017c003a33522fa681bf82348a60
root <root@Crunchbang.(none)>
2015-04-08 09:36:08 +0200

    idk

--- model/overzicht.model.php
+++ model/overzicht.model.php
@@ -8,7 +8,7 @@ parent::__construct();
 
 public function getBedrijven($query = null) {
 if($query == null){
-$sth = $this->dbh->prepare('SELECT * FROM bedrijven');
+$sth = $this->dbh->prepare('SELECT * FROM (SELECT * FROM bedrijven ORDER BY bedrijf_id DESC) x GROUP BY ref_nummer ORDER BY bedrijf_id ASC');
 } else{
 $sth = $this->dbh->prepare($query);
 }
