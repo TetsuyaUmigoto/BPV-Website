229cc5d52e8857dc01de16508dfd01f20797cb6c
Lubbert Kramer <lkramer@ict-zwolle.nl>
2015-05-18 10:59:51 +0200

    commit

--- model/leraren.model.php
+++ model/leraren.model.php
@@ -5,13 +5,13 @@ class Leraren_Model extends Model{
     public function __construct(){
 	parent::__construct();
     }
-    
+
     public function getMapsLocations($klasNaam){
 	$date = time();
 	$sth = $this->dbh->prepare("SELECT studenten.voornaam, studenten.achternaam, studenten.leerlingnummer,
 		pokaanvraag.leerlingnummer, pokaanvraag.bedrijfPostcode, pokaanvraag.bpvPeriodeBegin, pokaanvraag.bpvPeriodeEind
 		FROM studenten LEFT OUTER JOIN pokaanvraag ON studenten.leerlingnummer = pokaanvraag.leerlingnummer
-		WHERE ".$date." >= pokaanvraag.bpvPeriodeBegin AND ".$date." <= pokaanvraag.bpvPeriodeEind AND pokaanvraag.pokStatus = 3");
+		WHERE " . $date . " >= pokaanvraag.bpvPeriodeBegin AND " . $date . " <= pokaanvraag.bpvPeriodeEind AND pokaanvraag.pokStatus = 3");
 	$sth->execute();
 	return $sth->fetchall();
     }
@@ -64,7 +64,7 @@ class Leraren_Model extends Model{
 	$sth->execute();
 	return $sth->fetchall();
     }
-    
+
     public function getStudent($leerlingNummer){
 	$sth = $this->dbh->prepare("SELECT studenten.*, leraren.*, pokaanvraag.* "
 		. "			FROM studenten"
@@ -79,37 +79,37 @@ class Leraren_Model extends Model{
 	));
 	return $sth->fetchall();
     }
-    
+
     public function savePokChanges(){
-        $beginStage = strtotime($_POST['bpvPeriodeBegin']);
+	$beginStage = strtotime($_POST['bpvPeriodeBegin']);
 	$eindStage = strtotime($_POST['bpvPeriodeEind']);
-        
-        $sth = $this->dbh->prepare("UPDATE pokaanvraag SET bedrijfNaam = :bedrijfNaam, bedrijfAdres = :bedrijfAdres, bedrijfPostcode = :bedrijfPostcode, bedrijfPlaats = :bedrijfPlaats,
+
+	$sth = $this->dbh->prepare("UPDATE pokaanvraag SET bedrijfNaam = :bedrijfNaam, bedrijfAdres = :bedrijfAdres, bedrijfPostcode = :bedrijfPostcode, bedrijfPlaats = :bedrijfPlaats,
                                     bedrijfTelefoon = :bedrijfTelefoon, bedrijfWebsite = :bedrijfWebsite, bedrijfContactPersoon = :bedrijfContactPersoon, bedrijfContactPersoonTelefoon = :bedrijfContactPersoonTelefoon,
                                     bedrijfContactPersoonEmail = :bedrijfContactPersoonEmail, bedrijfPraktijkBegeleider = :bedrijfPraktijkBegeleider, bedrijfPraktijkBegeleiderTelefoon = :bedrijfPraktijkBegeleiderTelefoon,
                                     bedrijfKennisCentrum = :bedrijfKennisCentrum, bedrijfCode = :bedrijfCode, studentNaam = :studentNaam, studentKlas = :studentKlas, studentOpleiding = :studentOpleiding,
                                     studentCreboNummerOpleiding = :studentCreboNummerOpleiding, studentRichting = :studentRichting, studentInleverdatum = :studentInleverdatum, onderwijsBegeleider = :onderwijsBegeleider,
                                     vertegenwoordigerLandstede = :vertegenwoordigerLandstede, bpvPeriodeBegin = :bpvPeriodeBegin, bpvPeriodeEind = :bpvPeriodeEind, bpvSbu = :bpvSbu, werkprocessen = :werkprocessen, begeleidingTeam = :begeleidingTeam,
                                     begeleidingBedrijf = :begeleidingBedrijf WHERE leerlingnummer = :leerlingnummer ORDER BY id DESC LIMIT 1");
-        $sth->execute(array(
-            ':leerlingnummer' => $_POST['studentNummer'], ':bedrijfNaam' => $_POST['bedrijfNaam'],
-            ':bedrijfAdres' => $_POST['bedrijfAdres'], ':bedrijfPostcode' => $_POST['bedrijfPostcode'],
-            ':bedrijfPlaats' => $_POST['bedrijfPlaats'], ':bedrijfTelefoon' => $_POST['bedrijfTelefoon'],
-            ':bedrijfWebsite' => $_POST['bedrijfWebsite'], ':bedrijfContactPersoon' => $_POST['bedrijfContactpersoon'],
-            ':bedrijfContactPersoonTelefoon' => $_POST['bedrijfContactpersoonTelefoon'], ':bedrijfContactPersoonEmail' => $_POST['bedrijfContactpersoonEmail'],
-            ':bedrijfPraktijkBegeleider' => $_POST['bedrijfPraktijkbegeleider'], ':bedrijfPraktijkBegeleiderTelefoon' => $_POST['bedrijfPraktijkbegeleiderTelefoon'],
-            ':bedrijfKennisCentrum' => $_POST['bedrijfKenniscentrum'], ':bedrijfCode' => $_POST['bedrijfBedrijfscode'],
-            ':studentNaam' => $_POST['studentNaam'], ':studentKlas' => $_POST['studentKlas'],
-            ':studentOpleiding' => $_POST['studentOpleiding'], ':studentCreboNummerOpleiding' => $_POST['studentCrebonummerOpleiding'],
-            ':studentRichting' => $_POST['studentRichting'], ':studentInleverdatum' => $_POST['studentInleverdatum'],
-            ':vertegenwoordigerLandstede' => $_POST['vertegenwoordigerLandstede'], ':onderwijsBegeleider' => $_POST['onderwijsBegeleider'],
-            ':bpvPeriodeBegin' => $beginStage, ':bpvPeriodeEind' => $eindStage,
-            ':werkprocessen' => $_POST['werkprocessen'], ':begeleidingTeam' => $_POST['begeleidingTeam'],
-            ':bpvSbu' => $_POST['bpvSbu'], ':begeleidingBedrijf' => $_POST['begeleidingBedrijf']
-        ));
-        header("location: " . URL . "leraren");
+	$sth->execute(array(
+	    ':leerlingnummer' => $_POST['studentNummer'], ':bedrijfNaam' => $_POST['bedrijfNaam'],
+	    ':bedrijfAdres' => $_POST['bedrijfAdres'], ':bedrijfPostcode' => $_POST['bedrijfPostcode'],
+	    ':bedrijfPlaats' => $_POST['bedrijfPlaats'], ':bedrijfTelefoon' => $_POST['bedrijfTelefoon'],
+	    ':bedrijfWebsite' => $_POST['bedrijfWebsite'], ':bedrijfContactPersoon' => $_POST['bedrijfContactpersoon'],
+	    ':bedrijfContactPersoonTelefoon' => $_POST['bedrijfContactpersoonTelefoon'], ':bedrijfContactPersoonEmail' => $_POST['bedrijfContactpersoonEmail'],
+	    ':bedrijfPraktijkBegeleider' => $_POST['bedrijfPraktijkbegeleider'], ':bedrijfPraktijkBegeleiderTelefoon' => $_POST['bedrijfPraktijkbegeleiderTelefoon'],
+	    ':bedrijfKennisCentrum' => $_POST['bedrijfKenniscentrum'], ':bedrijfCode' => $_POST['bedrijfBedrijfscode'],
+	    ':studentNaam' => $_POST['studentNaam'], ':studentKlas' => $_POST['studentKlas'],
+	    ':studentOpleiding' => $_POST['studentOpleiding'], ':studentCreboNummerOpleiding' => $_POST['studentCrebonummerOpleiding'],
+	    ':studentRichting' => $_POST['studentRichting'], ':studentInleverdatum' => $_POST['studentInleverdatum'],
+	    ':vertegenwoordigerLandstede' => $_POST['vertegenwoordigerLandstede'], ':onderwijsBegeleider' => $_POST['onderwijsBegeleider'],
+	    ':bpvPeriodeBegin' => $beginStage, ':bpvPeriodeEind' => $eindStage,
+	    ':werkprocessen' => $_POST['werkprocessen'], ':begeleidingTeam' => $_POST['begeleidingTeam'],
+	    ':bpvSbu' => $_POST['bpvSbu'], ':begeleidingBedrijf' => $_POST['begeleidingBedrijf']
+	));
+	header("location: " . URL . "leraren");
     }
-    
+
     public function pokGoed($id){
 	$sth = $this->dbh->prepare("UPDATE pokaanvraag SET pokStatus = 1 WHERE id = :id");
 	$sth->execute(array(
@@ -117,13 +117,13 @@ class Leraren_Model extends Model{
 	));
 	header("location: " . URL . "leraren");
     }
-    
+
     public function afspraken($leerlingNummer){
 	$sth = $this->dbh->prepare("SELECT * FROM calender WHERE leerlingnummer = :leerlingnummer");
 	$sth->execute(array(
 	    ':leerlingnummer' => $leerlingNummer
 	));
-	
+
 	$aantalAfspraken = $sth->rowCount();
 	$afsprakenData = $sth->fetchall();
 	$afspraken = array();
