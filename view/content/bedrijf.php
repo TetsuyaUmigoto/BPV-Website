f001504ef5d4c6a70d314c22e1fd399b35676545
Henk <Henk@10.54.232.78>
2015-06-10 13:59:20 +0200

    map bedrijf

--- view/content/bedrijf.php
+++ view/content/bedrijf.php
@@ -1,43 +1,43 @@
 <div id="wrap">
     <div id="info"><h3>
-	    <?php
-	    if(isset($this->info)){
-		foreach($this->info as $info){
-		    ?>
-		    <div class="bedrijfInfo"><p><?php echo $info['bedrijf_naam'] ?></p></div>
-		    <div class="bedrijfInfo"><p id="Gsterren"><?php echo $this->gemiddelde ?> Sterren</p></div>
-		    <div class="bedrijfInfo"></div>
-		</h3> </div>
-	    <div id="infoWrapper">
+            <?php
+            if (isset($this->info)) {
+                foreach ($this->info as $info) {
+                    ?>
+                    <div class="bedrijfInfo"><p><?php echo $info['bedrijf_naam'] ?></p></div>
+                    <div class="bedrijfInfo"><p id="Gsterren"><?php echo $this->gemiddelde ?> Sterren</p></div>
+                    <div class="bedrijfInfo"></div>
+                </h3> </div>
+            <div id="infoWrapper">
 
-		<div id="comments">
+                <div id="comments">
 
-		    <div class="bedrijfInform"><p>Reactie's:</p></div>
-		</div>    
-		<?php
-	    }
-	}
-	?>
+                    <div class="bedrijfInform"><p>Reactie's:</p></div>
+                </div>    
+                <?php
+            }
+        }
+        ?>
 
-	<div id="gebruikerComments">
-	    <?php
-	    if(isset($this->comments)){
-		foreach($this->comments as $row){
-		    echo
-		    "<div class='leerlingnummer'>" . "Student: " . $row['voornaam'] . " " . $row['achternaam'] . "</div>"
-		    . " <div class='rating'>" . "Rating: " . $row['rating'] . "</div>"
-		    . "<div class='comment'>" . "Reactie: <br>" . $row['comment'] . "</div>" . "<br>";
-		}
-	    }
-	    ?>
-	</div></div>
+        <div id="gebruikerComments">
+            <?php
+            if (isset($this->comments)) {
+                foreach ($this->comments as $row) {
+                    echo
+                    "<div class='leerlingnummer'>" . "Student: " . $row['voornaam'] . " " . $row['achternaam'] . "</div>"
+                    . " <div class='rating'>" . "Rating: " . $row['rating'] . "</div>"
+                    . "<div class='comment'>" . "Reactie: <br>" . $row['comment'] . "</div>" . "<br>";
+                }
+            }
+            ?>
+        </div></div>
     <?php
-	if($_SESSION['role'] == 0){
-	    echo
-		"<div class='bedrijf-comments-test'>
-		    <form class='form2' action='".URL."bedrijf/postComment' method='post'>
+    if ($_SESSION['role'] == 0) {
+        echo
+        "<div class='bedrijf-comments-test'>
+		    <form class='form2' action='" . URL . "bedrijf/postComment' method='post'>
 		    <input type='hidden' name='leerlingnummer' value=" . $_SESSION['user_id'] . ">
-			<input type='hidden' name='bedrijf_id' value=".$this->bedrijfId." />
+			<input type='hidden' name='bedrijf_id' value=" . $this->bedrijfId . " />
 			<input type='hidden' name='rating' id='ratingInput' value='' />
 			Rating:<span class='rating'>
 			<input type='radio' class='rating-input'
@@ -60,73 +60,74 @@
 			<input type='submit' class='btnComment' value='Comment!'>
 		    </form>
 		</div>";
-	}else{
-	}
+    } else {
+        
+    }
     ?>
+    <div class="map2"><div id="map"  style="width: 500px; height: 400px;">
+        </div></div>
 </div>
-<div class="map2"><div id="map"  style="width: 500px; height: 400px;">
 
 
-	<script type="text/javascript"
-		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGBByvGgOTRGqO-ZB1hVoAeVZGjm6UoTM">
-	</script>
-	<script type="text/javascript">
-	    var delay = 100;
-	    var infowindow = new google.maps.InfoWindow();
-	    var latlng = new google.maps.LatLng(52.527978100000000000, 5.995350800000006000);
-	    var locations = <?php echo $this->locations ?>;
-	    var mapOptions = {
-		zoom: 7,
-		center: latlng,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	    }
-	    var geocoder = new google.maps.Geocoder();
-	    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	    var bounds = new google.maps.LatLngBounds();
+<script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCGBByvGgOTRGqO-ZB1hVoAeVZGjm6UoTM">
+</script>
+<script type="text/javascript">
+    var delay = 100;
+    var infowindow = new google.maps.InfoWindow();
+    var latlng = new google.maps.LatLng(52.527978100000000000, 5.995350800000006000);
+    var locations = <?php echo $this->locations ?>;
+    var mapOptions = {
+        zoom: 7,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var geocoder = new google.maps.Geocoder();
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var bounds = new google.maps.LatLngBounds();
 
-	    function geocodeAddress(address, next) {
-		geocoder.geocode({address: address}, function (results, status)
-		{
-		    if (status == google.maps.GeocoderStatus.OK) {
-			var p = results[0].geometry.location;
-			var lat = p.lat();
-			var lng = p.lng();
-			createMarker(address, lat, lng);
-		    }
-		    else {
-			if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-			    nextAddress--;
-			    delay++;
-			} else {
-			}
-		    }
-		    next();
-		}
-		);
-	    }
-	    function createMarker(add, lat, lng) {
-		var contentString = add;
-		var marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(lat, lng),
-		    map: map,
-		});
+    function geocodeAddress(address, next) {
+        geocoder.geocode({address: address}, function (results, status)
+        {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var p = results[0].geometry.location;
+                var lat = p.lat();
+                var lng = p.lng();
+                createMarker(address, lat, lng);
+            }
+            else {
+                if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+                    nextAddress--;
+                    delay++;
+                } else {
+                }
+            }
+            next();
+        }
+        );
+    }
+    function createMarker(add, lat, lng) {
+        var contentString = add;
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            map: map,
+        });
 
-		google.maps.event.addListener(marker, 'click', function () {
-		    infowindow.setContent(contentString);
-		    infowindow.open(map, marker);
-		});
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.setContent(contentString);
+            infowindow.open(map, marker);
+        });
 
-		bounds.extend(marker.position);
+        bounds.extend(marker.position);
 
-	    }
+    }
 
-	    var nextAddress = 0;
-	    function theNext() {
-		if (nextAddress < locations.length) {
-		    setTimeout('geocodeAddress("' + locations[nextAddress] + '",theNext)', delay);
-		    nextAddress++;
-		}
-	    }
-	    theNext();
-	</script>
-    </div></div>
+    var nextAddress = 0;
+    function theNext() {
+        if (nextAddress < locations.length) {
+            setTimeout('geocodeAddress("' + locations[nextAddress] + '",theNext)', delay);
+            nextAddress++;
+        }
+    }
+    theNext();
+</script>
