7dd2289b62f18b4c4aab979d6575e89d70cda020
Lubbert Kramer <lkramer@ict-zwolle.nl>
2015-06-17 14:33:47 +0200

    544t

--- docs/bpv-website.sql
+++ docs/bpv-website.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Genereertijd: 18 mei 2015 om 11:53
+-- Genereertijd: 17 jun 2015 om 14:33
 -- Serverversie: 5.6.14
 -- PHP-versie: 5.5.6
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `bedrijven` (
   `Praktijkbegeleider` varchar(30) NOT NULL,
   `Praktijkbegeleider_telefoon` int(10) NOT NULL,
   PRIMARY KEY (`bedrijf_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `bedrijven`
@@ -78,7 +78,8 @@ INSERT INTO `bedrijven` (`bedrijf_id`, `ref_nummer`, `soort`, `bedrijf_naam`, `A
 (30, 9504620, 0, 'Aannemingsmaatschappij va', 'J.P. Broekhovenstraat 36 ', '8081 HC', 'ELBURG', 525, ' http://www.gelder.com ', 0, ' m.dejonghe@gelder.comÂ ', 'Mw. J. (Jelly) Oost ', 0),
 (31, 9812355, 0, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', 2147483647, '  ', 2147483647, ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', 2147483647),
 (32, 9812355, 0, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', 2147483647, '  ', 2147483647, ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', 2147483647),
-(33, 9812355, 0, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', 2147483647, '  ', 2147483647, ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', 2147483647);
+(33, 9812355, 0, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', 2147483647, '  ', 2147483647, ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', 2147483647),
+(34, 9812355, 0, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', 2147483647, '  ', 2147483647, ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -88,23 +89,30 @@ INSERT INTO `bedrijven` (`bedrijf_id`, `ref_nummer`, `soort`, `bedrijf_naam`, `A
 
 CREATE TABLE IF NOT EXISTS `calender` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `timestamp` int(32) NOT NULL,
   `afspraak` varchar(500) NOT NULL,
   `leerlingnummer` int(11) NOT NULL,
   `leraren_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `calender`
 --
 
 INSERT INTO `calender` (`id`, `timestamp`, `afspraak`, `leerlingnummer`, `leraren_id`) VALUES
-(2, '2015-09-11 22:00:00', 'test', 1, 1),
-(3, '0000-00-00 00:00:00', 'test', 9, 1),
-(4, '0000-00-00 00:00:00', 'test', 9, 1),
-(5, '0000-00-00 00:00:00', '1', 9, 1),
-(6, '0000-00-00 00:00:00', 'anus', 3, 1);
+(2, 2147483647, 'test', 1, 1),
+(3, 0, 'test', 9, 1),
+(4, 0, 'test', 9, 1),
+(5, 0, '1', 9, 1),
+(6, 0, 'anus', 3, 1),
+(14, 1422745200, '', 2, 1),
+(15, 1422745201, '', 2, 1),
+(16, 1422752400, '', 2, 1),
+(17, 702950400, 'koekjes', 6, 1),
+(18, 697078800, 'terst', 6, 1),
+(19, 704937600, 'aids', 6, 1),
+(20, 694314000, '', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -119,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `comments` (
   `comment` varchar(500) NOT NULL,
   `rating` int(1) NOT NULL,
   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `comments`
@@ -128,7 +136,10 @@ CREATE TABLE IF NOT EXISTS `comments` (
 INSERT INTO `comments` (`comment_id`, `bedrijf_id`, `leerlingnummer`, `comment`, `rating`) VALUES
 (10, 26, 1, 'test', 2),
 (11, 26, 2, 'test2', 5),
-(12, 21, 1, 'test', 3);
+(12, 21, 1, 'test', 3),
+(13, 21, 1, '', 0),
+(14, 25, 1, 'rsedghgfd', 4),
+(15, 25, 1, 'sdfdgwer', 2);
 
 -- --------------------------------------------------------
 
@@ -190,7 +201,7 @@ CREATE TABLE IF NOT EXISTS `pokaanvraag` (
   `begeleidingBedrijf` varchar(240) NOT NULL,
   `pokStatus` int(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=45 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `pokaanvraag`
@@ -198,14 +209,15 @@ CREATE TABLE IF NOT EXISTS `pokaanvraag` (
 
 INSERT INTO `pokaanvraag` (`id`, `leerlingnummer`, `bedrijfNaam`, `bedrijfAdres`, `bedrijfPostcode`, `bedrijfPlaats`, `bedrijfTelefoon`, `bedrijfWebsite`, `bedrijfContactPersoon`, `bedrijfContactPersoonTelefoon`, `bedrijfContactPersoonEmail`, `bedrijfPraktijkBegeleider`, `bedrijfPraktijkBegeleiderTelefoon`, `bedrijfKennisCentrum`, `bedrijfCode`, `studentNaam`, `studentKlas`, `studentOpleiding`, `studentCreboNummerOpleiding`, `studentRichting`, `studentInleverdatum`, `onderwijsBegeleider`, `vertegenwoordigerLandstede`, `bpvPeriodeBegin`, `bpvPeriodeEind`, `bpvSbu`, `werkprocessen`, `begeleidingTeam`, `begeleidingBedrijf`, `pokStatus`) VALUES
 (33, 6, '12Build', 'Nijverdalseweg 143 ', '7461 AG', 'RIJSSEN', '548', ' http://www.12build.com ', 'Dhr. R.G.J. (Robert) van Bloem ', '548', ' r.vanbloem@12build.comÂ ', 'Dhr. R.G.J. (Robert) van Bloem ', '548', '', '', 'Jonas Muilwijk', 'IT13A', '', '', '', '', '', '', 0, 0, 0, '', '', '', 0),
-(36, 4, '2Complex B.V.', 'K J Blokstraat 49 ', '8384 EV', 'WILHELMINAOORD', '521', '  ', 'Dhr. C. Hilbrink ', '521', ' cor@hilbrink.nlÂ ', 'Dhr. C. Hilbrink ', '521', '', '', 'kaas test', '321', '', '', '', '', '', '', 0, 0, 0, '', '', '', 1),
+(36, 4, '2Complex B.V.', 'K J Blokstraat 49 ', '8384 EV', 'WILHELMINAOORD', '521', '  ', 'Dhr. C. Hilbrink ', '521', ' cor@hilbrink.nlÂ ', 'Dhr. C. Hilbrink ', '521', '', '', 'test kaas', 'IT13A', '', '', '', '', '', '', 0, 0, 0, '', '', '', 1),
 (37, 8, '2G BV', 'Essenkamp 2 ', '3888 LL', 'UDDEL', '577', ' http://www.2g.it.nl ', 'Dhr. J.C. (Joost) Geuze ', '577', ' jcgeuze@2g-it.nlÂ ', 'Dhr. J.C. (Joost) Geuze ', '577', '', '', 'kees gup', '1', '', '', '', '', '', '', 1423695600, 1531640800, 0, '', '', '', 3),
 (38, 3, '4ITpoint', 'Dokter van Deenweg 13 ', '8025 BP', 'ZWOLLE', '38', ' http://www.4itpoint.nl ', 'Dhr. E. (Eric) Eller ', '38', ' eeller@4itpoint.nlÂ ', 'Dhr. E. (Eric) Eller ', '38', '', '', 'Lubbert Kramer', 'IT13A', '', '', '', '', 'test', 'test', 0, 0, 0, 'test', 'test', 'test', 0),
 (39, 5, '8internet', 'Schweitzerstraat 31 ', '7909 AV', 'HOOGEVEEN', '528', ' http://8internet.nl/ ', 'Dhr. R.C. (Ramon) Mol ', '528', ' info@8internet.nlÂ ', 'Dhr. R.C. (Ramon) Mol ', '528', '', '', 'kop tulp', '1', '', '', '', '', '', '', 0, 0, 0, '', '', '', 0),
 (40, 7, 'A-Vision', 'IJzerweg 53 ', '7335 DH', 'APELDOORN', '55', ' http://www.a-vision.nu ', 'Mw. M. (Marjan) van Kleef ', '55', ' mvankleef@a-vision.nuÂ ', 'Mw. H. (Hermien) Lubbers-Jurrien ', '55', '', '', 'tulp kop', '1', '', '', '', '', '', '', 1423695600, 1531640800, 0, '', '', '', 3),
 (41, 2, '2 Monkeys', 'Laboratoriumplein 43 ', '7411 CH', 'DEVENTER', '6', '  ', '', '0', '', '', '0', '', '', 'Stefan Koekdeeg', 'IT13A', '', '', '', '', '', '', -3600, -3600, 0, '', '', '', 0),
 (42, 9, 'input bedrijf naam hier', 'J.P. Broekhovenstraat 36 ', '8081 HC', 'ELBURG', '0525-659888', ' http://www.gelder.com ', 'Mw. M. (Mariet) de Jonghe ', 'Â ', ' m.dejonghe@gelder.comÂ ', 'Mw. J. (Jelly) Oost ', 'Â ', '', '', 'Jonas Muilwijk', 'IT13A', '', '', '', '', '', '', 1423695600, 1531640800, 0, '', '', '', 3),
-(43, 1, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', '+31308506080', '  ', 'Dhr. C. (Cyril) Loosjes ', '+31308506080Â ', ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', '+31308506080Â ', '', '', 'Jonas Muilwijk', 'IT13A', '', '', '', '', '', '', 0, 0, 0, '', '', '', 1);
+(43, 1, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', '+31308506080', '  ', 'Dhr. C. (Cyril) Loosjes ', '+31308506080Â ', ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', '+31308506080Â ', '', '', 'Jonas Muilwijk', 'IT13A', '', '', '', '', '', '', 0, 0, 0, '', '', '', 1),
+(44, 1, '!CL Web', 'Winthontlaan 200 ', '3526 KV', 'UTRECHT', '+31308506080', '  ', 'Dhr. C. (Cyril) Loosjes ', '+31308506080Â ', ' info@clweb.nlÂ ', 'Dhr. C. (Cyril) Loosjes ', '+31308506080Â ', '', '', 'Jonas Muilwijk', 'IT13A', '', '', '', '', '', '', 0, 0, 0, '', '', '', 0);
 
 -- --------------------------------------------------------
 
